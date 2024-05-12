fc049190c4ae0839bdd3efb0fd20162ea22237dc
JuliaChurzina <julia.churzina@icons8.com>
2024-05-12 09:29:32 +0300

    two_lesson

---
+++ google_should_find_selene.py
@@ -0,0 +1,37 @@
+from selene import browser, be, have
+
+
+def test_should_text():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_should_not_text():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.no.text('qwerty'))
+
+
+def test_should_text_ya():
+    browser.open('https://ya.ru/')
+    browser.element('[name="text"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search-result"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
+def test_should_not_text_ya():
+    browser.open('https://ya.ru/')
+    browser.element('[name="text"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search-result"]').should(have.no.text('qwerty'))
+
+
+def test_should_text_duck():
+    browser.open('https://duckduckgo.com/')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="react-layout"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
+def test_should_not_text_duck():
+    browser.open('https://duckduckgo.com/')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="react-layout"]').should(have.no.text('qwerty'))
